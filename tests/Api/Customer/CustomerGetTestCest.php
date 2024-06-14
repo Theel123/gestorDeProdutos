5fb0960d64660fe1ea5a127018ae15743515883a
Theel123 <theel.na@hotmail.com>
2024-06-14 14:47:32 -0300

    feature-006 | Adding More Tests To Analyze User Admin Rules In Customer EndPoints

--- tests/Api/Customer/CustomerGetTestCest.php
+++ tests/Api/Customer/CustomerGetTestCest.php
@@ -9,6 +9,7 @@ class CustomerGetTestCest
 {
     private $url = "http://localhost:9011/api/v1/customer";
     private string $tokenLoginAdmin;
+    private string $tokenLoginUser;
 
     // tests
 
@@ -22,6 +23,14 @@ class CustomerGetTestCest
         $this->tokenLoginAdmin = $testHelper::login(
             [
                 'email' => 'admin@cellar.com',
+                'password' => 'password',
+            ],
+            $I
+        );
+
+        $this->tokenLoginUser = $testHelper::login(
+            [
+                'email' => 'user@cellar.com',
                 'password' => 'password',
             ],
             $I
@@ -49,5 +58,25 @@ class CustomerGetTestCest
         $I->sendGet(sprintf("$this->url/%s", '1'));
 
         $I->seeResponseCodeIs(HttpCode::UNAUTHORIZED);
+    }
+
+    public function ifUserIsAdminAccessingOtherUsers(ApiTester $I)
+    {
+        $I->wantTo('Test If User Is Admin Accessing Other Users');
+
+        $I->sendGet(sprintf("$this->url/%s", '1'));
+
+        $I->seeResponseCodeIs(HttpCode::OK);
+    }
+
+    public function ifUserComumAccessingAnotherUser(ApiTester $I)
+    {
+        $I->wantTo('Test User Comum Accessing Another User');
+
+        $I->amBearerAuthenticated($this->tokenLoginUser);
+
+        $I->sendGet(sprintf("$this->url/%s", '1'));
+
+        $I->seeResponseCodeIs(HttpCode::BAD_REQUEST);
     }
 }
